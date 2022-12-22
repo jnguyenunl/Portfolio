28a127e062174117510a5151ca04fabc83114b8a
jamesnguyen36 <81396832+jnguyenunl@users.noreply.github.com>
2022-12-22 15:53:27 -0600

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,44 +4,44 @@ import contactImg from "../assets/img/contact-img.svg";
 import colorSharp from "../assets/img/color-sharp.png";
 
 const Contact = () => {
-  const formInitialDetails = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    message: "",
-  };
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState("Send");
-  const [status, setStatus] = useState({});
+  // const formInitialDetails = {
+  //   firstName: "",
+  //   lastName: "",
+  //   email: "",
+  //   phone: "",
+  //   message: "",
+  // };
+  // const [formDetails, setFormDetails] = useState(formInitialDetails);
+  // const [buttonText, setButtonText] = useState("Send");
+  // const [status, setStatus] = useState({});
 
-  const onFormUpdate = (catergory, value) => {
-    setFormDetails({ ...formDetails, [catergory]: value });
-  };
+  // const onFormUpdate = (catergory, value) => {
+  //   setFormDetails({ ...formDetails, [catergory]: value });
+  // };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("https://jamesnguyenportfolio.netlify.app/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code === 200) {
-      setStatus({ succes: true, message: "Message sent successfully!" });
-    } else {
-      setStatus({
-        succes: false,
-        message: "Something went wrong, please try again later.",
-      });
-    }
-  };
-//   "http://localhost:3000/contact"
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setButtonText("Sending...");
+  //   let response = await fetch("https://jamesnguyenportfolio.netlify.app/contact", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json;charset=utf-8",
+  //     },
+  //     body: JSON.stringify(formDetails),
+  //   });
+  //   setButtonText("Send");
+  //   let result = await response.json();
+  //   setFormDetails(formInitialDetails);
+  //   if (result.code === 200) {
+  //     setStatus({ succes: true, message: "Message sent successfully!" });
+  //   } else {
+  //     setStatus({
+  //       succes: false,
+  //       message: "Something went wrong, please try again later.",
+  //     });
+  //   }
+  // };
+  //   "http://localhost:3000/contact"
 
   return (
     <section className="contact" id="connect">
@@ -51,70 +51,61 @@ const Contact = () => {
           <Col md={6}>
             <img src={contactImg} alt="Contact" />
           </Col>
-          {status.message ? (
-            <Col>
-              <p className={status.succes === false ? "danger" : "success"}>
-                {status.message}
-              </p>
-            </Col>
-          ) : (
-            <Col>
-              <form onSubmit={handleSubmit}>
-                <Row>
-                  <Col sm={6} className="px-1">
-                    <input
-                      type="text"
-                      value={formDetails.firstName}
-                      placeholder="First Name"
-                      onChange={(e) =>
-                        onFormUpdate("firstName", e.target.value)
-                      }
-                      required
-                    />
-                  </Col>
-                  <Col sm={6} className="px-1">
-                    <input
-                      type="text"
-                      value={formDetails.lastName}
-                      placeholder="Last Name"
-                      onChange={(e) => onFormUpdate("lastName", e.target.value)}
-                      required
-                    />
-                  </Col>
-                  <Col sm={6} className="px-1">
-                    <input
-                      type="email"
-                      value={formDetails.email}
-                      placeholder="Email Address"
-                      onChange={(e) => onFormUpdate("email", e.target.value)}
-                      required
-                    />
-                  </Col>
-                  <Col sm={6} className="px-1">
-                    <input
-                      type="tel"
-                      value={formDetails.phone}
-                      placeholder="Phone Number"
-                      pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-                      onChange={(e) => onFormUpdate("phone", e.target.value)}
-                    />
-                  </Col>
-                  <Col size={12} className="px-1">
-                    <textarea
-                      row="6"
-                      value={formDetails.message}
-                      placeholder="Message"
-                      onChange={(e) => onFormUpdate("message", e.target.value)}
-                      required
-                    />
-                    <button>
-                      <span>{buttonText}</span>
-                    </button>
-                  </Col>
-                </Row>
-              </form>
-            </Col>
-          )}
+          <Col>
+            <form name="contact" method="POST" data-netlify="true">
+              <Row>
+                <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    // value={formDetails.firstName}
+                    placeholder="First Name"
+                    // onChange={(e) => onFormUpdate("firstName", e.target.value)}
+                    required
+                    name="name"
+                  />
+                </Col>
+                {/* <Col sm={6} className="px-1">
+                  <input
+                    type="text"
+                    // value={formDetails.lastName}
+                    placeholder="Last Name"
+                    // onChange={(e) => onFormUpdate("lastName", e.target.value)}
+                    required
+                  />
+                </Col> */}
+                <Col sm={6} className="px-1">
+                  <input
+                    type="email"
+                    // value={formDetails.email}
+                    placeholder="Email Address"
+                    // onChange={(e) => onFormUpdate("email", e.target.value)}
+                    required
+                    name="email"
+                  />
+                </Col>
+                {/* <Col sm={6} className="px-1">
+                  <input
+                    type="tel"
+                    // value={formDetails.phone}
+                    placeholder="Phone Number"
+                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                    // onChange={(e) => onFormUpdate("phone", e.target.value)}
+                  />
+                </Col> */}
+                <Col size={12} className="px-1">
+                  <textarea
+                    row="6"
+                    // value={formDetails.message}
+                    placeholder="Message"
+                    // onChange={(e) => onFormUpdate("message", e.target.value)}
+                    required
+                    name="message"
+                  />
+                  <button type="submit">Send</button>
+                </Col>
+              </Row>
+            </form>
+          </Col>
         </Row>
       </Container>
       <img className="background-image-left" src={colorSharp} alt="null" />
