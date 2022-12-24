64a5c6100d82c3d059e2a33a5a2ea743de48ba80
jamesnguyen36 <81396832+jnguyenunl@users.noreply.github.com>
2022-12-23 23:10:58 -0600

    Update reCAPTCHA 2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,12 @@ const Contact = () => {
             <img src={contactImg} alt="Contact" />
           </Col>
           <Col>
-            <form name="form" method="post">
+            <form
+              name="form"
+              method="post"
+              data-netlify-recaptcha="true"
+              data-netlify="true"
+            >
               <input type="hidden" name="form-name" value="form" />
               <div hidden>
                 <input name="bot-field" />
